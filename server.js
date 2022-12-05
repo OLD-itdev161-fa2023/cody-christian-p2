c89faa8fe3ac512db56769aae66d393f42f7c9bd
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-12-04 20:37:50 -0600

    update user reg ep

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectDatabase from "./config/db";
 import { check, validationResult } from 'express-validator';
 import cors from "cors";
+import User from "./models/User";
+import bcrypt from "bcryptjs";
 
 
 const app = express();
@@ -29,26 +31,51 @@ app.get('/', (req,res) =>
  * @desc Register user
  */
 app.post(
-    '/api/users',
+    "/api/users",
     [
-        check('name', 'Please enter your name')
-        .not()
-        .isEmpty(),
-        check('email', 'Please enter a valid email').isEmail(),
+        check("name", "Please enter your name").not().isEmpty(),
+        check("email", "Please enter a valid email").isEmail(),
         check(
-        'password',
-        'Please enter a password with 6 or more characters'
-        ).isLength({ min: 6 })
+        "password",
+        "Please enter a password with 6 or more characters"
+        ).isLength({ min: 6 }),
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
         } else {
-        return res.send(req.body);
+        const { name, email, password } = req.body;
+        try {
+            // Check if user exists
+            let user = await User.findOne({ email: email });
+            if (user) {
+            return res
+                .status(400)
+                .json({ errors: [{ msg: "User already exists" }] });
+            }
+
+            // Create a new user
+            user = new User({
+                name: name,
+                email: email,
+                password: password,
+            });
+
+            // Encrypt the password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            // Save to the db and return
+            await user.save();
+            res.send('User successfully register');
+            } catch (error) {
+            res.status(500).send("Server error");
+            }
         }
     }
 );
+
 
 //listener
 const port = 5000;
